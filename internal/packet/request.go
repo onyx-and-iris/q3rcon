b20bca0c7783949c080bd21cce227d7c82df6e21
onyx-and-iris <code@onyxandiris.online>
2024-11-08 15:31:30 +0000

    Request now uses reusable buffer

--- internal/packet/request.go
+++ internal/packet/request.go
@@ -1,16 +1,23 @@
 package packet
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
+
+const bufSz = 512
 
 type Request struct {
 	magic    []byte
 	password string
+	buf      *bytes.Buffer
 }
 
 func NewRequest(password string) Request {
 	return Request{
 		magic:    []byte{'\xff', '\xff', '\xff', '\xff'},
 		password: password,
+		buf:      bytes.NewBuffer(make([]byte, bufSz)),
 	}
 }
 
@@ -19,7 +26,8 @@ func (r Request) Header() []byte {
 }
 
 func (r Request) Encode(cmd string) []byte {
-	datagram := r.Header()
-	datagram = append(datagram, fmt.Sprintf(" %s %s", r.password, cmd)...)
-	return datagram
+	r.buf.Reset()
+	r.buf.Write(r.Header())
+	r.buf.WriteString(fmt.Sprintf(" %s %s", r.password, cmd))
+	return r.buf.Bytes()
 }
